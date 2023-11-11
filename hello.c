a9fb3a2d63e2d08b3cdcb9499bf37343f9512309
jazevedo <joaoazevedo592@gmail.com>
2023-11-11 13:03:36 -0300

    Hello

---
+++ hello.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+static int	strsize(char *str)
+{
+	int	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+void	printer(char *str, char *new, int position)
+{
+	int	i = '\0';
+	int	timer = 0;
+
+	str++;
+	while (i <= *str)
+	{
+		if (timer == 10000000)
+		{
+			if (i == 1)
+				i = ' ';
+			if (i != '\0')
+				printf("%s%c\n", new, i);
+			timer = 0;
+			i++;
+		}
+		timer++;
+	}
+	new[position] = i - 1;
+	if (new[position] == '\0')
+		return ;
+	printer(str, new, position + 1);
+}
+
+int	main(void)
+{
+	char	str[] = "Hello World!";
+	char	new[strsize(str)];
+	int	max = strsize(str);
+	int	timer = 0;
+	int	i = '\0';
+
+	while (max--)
+		new[max] = 0;
+	while (i <= *str)
+	{
+		if (timer == 10000000)
+		{
+			if (i == 1)
+				i = ' ';
+			if (i != '\0')
+				printf("%c\n", i);
+			timer = 0;
+			i++;
+		}
+		timer++;
+	}
+	new[0] = i;
+	printer(str, new, 1);
+}
