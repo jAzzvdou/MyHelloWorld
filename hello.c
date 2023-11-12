d251d9e52fae42a7b6f7611a587a6f6538ba4a96
jAzzvdou <joaoazevedo592@gmail.com>
2023-11-12 19:08:29 -0300

    Hello

--- hello.c
+++ hello.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 
 static int	strsize(char *str)
 {
@@ -11,21 +12,16 @@ static int	strsize(char *str)
 void	printer(char *str, char *new, int position)
 {
 	int	i = '\0';
-	int	timer = 0;
 
 	str++;
 	while (i <= *str)
 	{
-		if (timer == 10000000)
-		{
-			if (i == 1)
-				i = ' ';
-			if (i != '\0')
-				printf("%s%c\n", new, i);
-			timer = 0;
-			i++;
-		}
-		timer++;
+		usleep(10000);
+		if (i == 1)
+			i = ' ';
+		if (i != '\0')
+			printf("%s%c\n", new, i);
+		i++;
 	}
 	new[position] = i - 1;
 	if (new[position] == '\0')
@@ -33,30 +29,38 @@ void	printer(char *str, char *new, int position)
 	printer(str, new, position + 1);
 }
 
-int	main(int argc, char *argv[])
+void	reader(char *str)
 {
-	(void)argc;
-	char	*str = argv[1];
 	char	new[strsize(str)];
-	int	max = strsize(str);
-	int	timer = 0;
-	int	i = '\0';
+        int	max = strsize(str);
+        int	i = '\0';
 
 	while (max--)
 		new[max] = 0;
 	while (i <= *str)
 	{
-		if (timer == 10000000)
-		{
-			if (i == 1)
-				i = ' ';
-			if (i != '\0')
-				printf("%c\n", i);
-			timer = 0;
-			i++;
-		}
-		timer++;
+		usleep(10000);
+		if (i == 1)
+			i = ' ';
+		if (i != '\0')
+			printf("%c\n", i);
+		i++;
 	}
 	new[0] = i - 1;
 	printer(str, new, 1);
+}
+
+int	main(int argc, char *argv[])
+{
+	char	*str;
+        int	n = 1;
+        int	c = argc;
+
+	(void)argc;
+	while (n < c)
+	{
+		str = argv[n];
+		reader(str);
+		n++;
+	}
 }
